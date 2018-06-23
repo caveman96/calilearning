15c19d046f8f5a6405242e36a773d705741772d4
caveman96 <silesh.c@gmail.com>
2018-06-23 12:51:06 +0530

    adding files, first commit

---
+++ main.py
@@ -0,0 +1,38 @@
+"""
+Created on Thu Jun 21 09:44:59 2018
+@author: Silesh Chandran
+"""
+
+# Common imports
+import numpy as np
+import os
+
+# to make this notebook's output stable across runs
+np.random.seed(42)
+
+# To plot pretty figures
+import matplotlib
+import matplotlib.pyplot as plt
+plt.rcParams['axes.labelsize'] = 14
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['ytick.labelsize'] = 12
+
+# Where to save the figures
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "end_to_end_project"
+IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID)
+
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
+
+import pandas as pd
+
+housing = pd.read_csv("housing.csv")
+
+housing["ocean_proximity"].value_counts()
+
+
